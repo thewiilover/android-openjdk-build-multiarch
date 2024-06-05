a34df5f094ad158928b7adb6b4cc546c162499ff
The Judge <53906078+thejudge156@users.noreply.github.com>
2024-06-05 14:33:28 -0700

    Update clonejdk.sh

--- clonejdk.sh
+++ clonejdk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-git clone --branch "jdk-21.0.3+9" --depth 1 https://github.com/openjdk/jdk21u openjdk
+git clone --branch "jdk-21.0.4+5" --depth 1 https://github.com/openjdk/jdk21u openjdk
