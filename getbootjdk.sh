5b8be7204c63c453e9694bb153f7c25b29db3a06
The Judge <53906078+thejudge156@users.noreply.github.com>
2024-06-05 17:07:43 -0700

    Update getbootjdk.sh

--- getbootjdk.sh
+++ getbootjdk.sh
@@ -9,8 +9,8 @@ set -e
 # Please change if you have different architecture.
 
 if [ "$BUILD_IOS" != "1" ]; then
-  wget https://download.java.net/java/GA/jdk20/bdc68b4b9cbc4ebcb30745c85038d91d/36/GPL/openjdk-20_linux-x64_bin.tar.gz
+  wget https://download.java.net/java/GA/jdk22.0.1/c7ec1332f7bb44aeba2eb341ae18aca4/8/GPL/openjdk-22.0.1_linux-x64_bin.tar.gz
 else
-  wget https://download.java.net/java/GA/jdk20/bdc68b4b9cbc4ebcb30745c85038d91d/36/GPL/openjdk-20_macos-aarch64_bin.tar.gz
+  wget https://download.java.net/java/GA/jdk22.0.1/c7ec1332f7bb44aeba2eb341ae18aca4/8/GPL/openjdk-22.0.1_macos-aarch64_bin.tar.gz
 fi
-tar xvf openjdk-20*.tar.gz
+tar xvf openjdk-22*.tar.gz
